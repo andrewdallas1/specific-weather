ffc2f12ee1db3277ca5de66bd886410d0e78b45a
andrew dallas <adal1@live.com>
2017-02-12 16:57:10 -0500

    Index bug fixes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+require('dotenv').config();
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  axios.post('https://www.googleapis.com/geolocation/v1/geolocate?key=' + `${process.env.GEO_API}`)
+  .then((geoRes) => {
+    res.locals.lat = geoRes.data.location.lat,
+    res.locals.long = geoRes.data.location.lng
+  }).then(() => {
+    return next();
+  })
+})
+
+
+
+router.get('/', (req, res, next) => {
+  axios.get('https://api.darksky.net/forecast/' + `${process.env.API_KEY}` + '/' + `${res.locals.lat}` + ',' + `${res.locals.long}`)
+  .then((weatherResults) => {
+    res.render('user/weather', {
+     summary: weatherResults.data.minutely.summary,
+     temp: weatherResults.data.currently.temperature,
+     icon: weatherResults.data.currently.icon
+    })
+  }).catch((err) => {console.log(err)})
 });
+
+
 
 module.exports = router;
