c6c948d983c4b152dc4b4b930c2abb8ce05266cc
andrew dallas <adal1@live.com>
2017-02-12 16:44:27 -0500

    heroku commit

---
+++ routes/users.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios');
+var weather_call = require('../calls/weather_call');
+var bodyParser = require('body-parser');
+var app = express();
+require('dotenv').config();
+
+/* GET users listing. */
+router.get('/', (req, res, next) => {
+  axios.post('https://www.googleapis.com/geolocation/v1/geolocate?key=' + `${process.env.GEO_API}`)
+  .then((geoRes) => {
+    res.locals.lat = geoRes.data.location.lat,
+    res.locals.long = geoRes.data.location.lng
+  }).then(() => {
+    return next();
+  })
+})
+
+
+
+router.get('/', (req, res, next) => {
+  axios.get('https://api.darksky.net/forecast/' + `${process.env.API_KEY}` + '/' + `${res.locals.lat}` + ',' + `${res.locals.long}`)
+  .then((weatherResults) => {
+    res.render('user/weather', {
+     summary: weatherResults.data.minutely.summary,
+     temp: weatherResults.data.currently.temperature,
+     icon: weatherResults.data.currently.icon
+    })
+  }).catch((err) => {console.log(err)})
+});
+
+
+
+module.exports = router;
